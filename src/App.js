518627a054b8a4863043afd48bed092b302dd4a4
Kariakito <pilarmaestre@gmail.com>
2022-02-07 09:30:26 +0100

    almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const initialProductList = [
+  { id: 1, name: 'product 1' , price: 50, quantity: 1 },
+  { id: 2, name: 'product 2', price: 75, quantity: 2 },
+  { id: 3, name: 'product 3', price: 20, quantity: 5 }
+];
+
+//console.log(initialProductList);
+
+
 
 function App() {
+  const [products, setProducts] = useState (initialProductList);
+
+  function calculateTotalPrice () {
+    let totalPrice = 0;
+
+    products.forEach((item) => {
+       totalPrice = totalPrice + item.price *item.quantity;
+
+    });
+
+    return totalPrice;
+  }
+
+  function renderProducts () {
+    return products.map((item) => {
+      return (
+        <tr>
+          <td>{item.name}</td>
+          <td>{item.price}</td>
+          <td>{item.quantity}</td>
+          <td>{item.price * item.quantity}</td>
+        </tr>
+      );
+    });
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Orders Table</h1>
+
+        <table>
+
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Qty </th>
+            <th> Item Total price and qty</th>
+          </tr>
+
+          </thead>
+          
+
+            <tbody>
+             {renderProducts()}
+            </tbody>
+          
+        </table>
+
+        <h4>total price of all the product in table= xxxx</h4>
+
+
+        <h3>Add Item</h3>
+
+        <label thmlFor="">Product</label>
+        <input type="text" />
+
+
+        <label thmlFor="">Price</label>
+        <input type="text" />
+
+        <button>Enter Product</button>
+
     </div>
   );
 }
